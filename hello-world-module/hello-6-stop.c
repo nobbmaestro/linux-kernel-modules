7ad9de59d317b1f555f55774cae881ef973043c5
Norbert Batiuk <norbert@batiuk.se>
2023-11-27 11:08:28 +0100

    Add hello-6 module

---
+++ hello-world-module/hello-6-stop.c
@@ -0,0 +1,13 @@
+/*
+ * hello-6-stop.c - Illustration of multi filed modules
+ */
+
+#include <linux/kernel.h> /* We are doing kernel work */
+#include <linux/module.h> /* Specifically, a module */
+
+void cleanup_module(void)
+{
+	pr_info("Short is the life of kernel module\n");
+}
+
+MODULE_LICENSE("GPL");
